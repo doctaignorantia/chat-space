ad802d66b63bc3d59ef663e1671ed5b051efe09f
takashi murakami <defence_in_depth@mopera.net>
2019-08-31 14:18:51 +0900

    devise(chat-space)
    file移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
+  # root 'messages#index'
+  root 'groups#index'
+  resource :user, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   end
-  get 'messages' => 'messages#index'
 end
