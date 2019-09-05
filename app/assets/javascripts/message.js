61a27b084f07949060516934e5e9d79912c30d02
takashi murakami <defence_in_depth@mopera.net>
2019-09-05 22:06:54 +0900

    console.log(this);(chat-space)
    とりあえず、console.log(this);で
    catch

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,40 @@
+$(function(){
+  function buildHTML(message){
+    var html = .message
+                .upper-message
+                  .upper-message__user-name
+                    = message.user.name
+                  .upper-message__date
+                    = message.created_at.strftime("%Y/%m/%d %H:%M")
+                .lower-message
+                  - if message.content.present?
+                    %p.lower-message__content
+                      = message.content
+                  = image_tag message.image.url, class: 'lower-message__image' if message.image.present?
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    // debugger
+    e.preventDefault();
+    console.log(this);
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    
+    // .done(function(data){
+    //   var html = buildHTML(data);
+    //   $('.comments').append(html)
+    //   $('.textbox').val('')
+    // })
+    // .fail(function(){
+    //   alert('error');
+    // })
+  });
+});
