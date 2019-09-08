1834495589aa4a7a1bb60889be340cc0460f961d
takashi murakami <defence_in_depth@mopera.net>
2019-09-08 21:38:43 +0900

    view 相当マシになった
    view 相当マシになった

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,44 +2,43 @@ $(document).on('turbolinks:load', function(){
 
 $(function() {
 
-  // var search_list = $("#user-search-result");
-  // var member_list = $("#member_search_result");
+  var search_list = $("#user-search-result");
+  var member_list = $("#member_search_result");
 
-  // function appendUsers(user) {
-  //   var html =`<div class="chat-group-user clearfix">
-  //               <p class="chat-group-user__name">${ user.name }</p>
-  //               <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</a>
-  //             </div>`
+  function appendUsers(user) {
+    var html =`
+        <div id ='user-search-result'>
+        <div class="chat-group-user clearfix">
+          <p class="chat-group-user__name">${ user.name }</p>
+          <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</div>
+        </div>  
+        </div>`
+    search_list.append(html);
+    return html;
+  }
 
-  //   search_list.append(html);
-  //   return html;
-  // }
+  function appendMembers(name, user_id) {
+    var html =`
+        <div id ='member_search_result'>
+        <div class='chat-group-user clearfix'>
+          <input name='group[user_ids][]' type='hidden' value=${ user_id }>
+          <p class='chat-group-user__name'>${ name }</p>
+          <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除
+          </div>
+        </div>
+        </div>`  
 
-  // function appendMembers(name, user_id) {
-  //             // <div class='chat-group-user'>
-  //             //   <div class='chat-group-form__field--left'>
-  //             //     <label class='chat-group-form__label' for='chat_group_チャットメンバー'>チャットメンバー</label>
-  //             //   </div>
-  //             // </div>
-  //   var html =`<div class='chat-group-user clearfix'>
-  //               <div class='chat-group-form__field--right'>
-  //                 <input name='group[user_ids][]' type='hidden' value=${ user_id }>
-  //                 <p class='chat-group-user__name'>${ name }</p>
-  //                 <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-  //               </div>
-  //             </div>`
+    member_list.append(html);
+    return html;
+  }
 
-  //   member_list.append(html);
-  //   return html;
-  // }
+  function appendNoUsers(info) {
+    var html =`<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ info }</p>
+              </div>`
 
-  // function appendNoUsers(info) {
-  //   var html =`<div class="chat-group-user clearfix">
-  //               <p class="chat-group-user__name">${ info }</p>
-  //             </div>`
-
-  //   search_list.append(html);
-  // }
+    search_list.append(html);
+  }
 
   $(function(){
     $("#user-search-field").on("keyup", function() {
