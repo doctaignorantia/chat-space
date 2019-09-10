13aff5ae28fe7b63012333fdaf86dde25eaf5bc1
takashi murakami <defence_in_depth@mopera.net>
2019-09-10 13:13:24 +0900

    9/10-2

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,81 +1,122 @@
 $(document).on('turbolinks:load', function(){
 
-$(function() {
+  $(function() {
 
-  var search_list = $("#user-search-result");
-  var member_list = $("#member_search_result");
+    var search_list = $("#user-search-result");
+    var member_list = $("#member_search_result");
 
-  function appendUsers(user) {
-    var html =`
-        <div id ='user-search-result'>
-        <div class="chat-group-user clearfix">
-          <p class="chat-group-user__name">${ user.name }</p>
-          <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</div>
-        </div>  
-        </div>`
-    search_list.append(html);
-  }
+    function appendUsers(user) {
+      var html =`
+          <div id ='user-search-result'>
+          <div class="chat-group-user clearfix">
+            <p class="chat-group-user__name">${ user.name }</p>
+            <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name=${ user.name }>追加</div>
+          </div>  
+          </div>`
+      search_list.append(html);
+    }
 
-  function appendMembers(name, user_id) {
-    var html =`
-        <div id ='member_search_result'>
-        <div class='chat-group-user clearfix'>
-          <p class='chat-group-user__name'>${ name }</p>
-          <input name='group[user_ids][]' type='hidden' value=${ user_id }>
-          <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除
+    function appendMembers(name, user_id) {
+      var html =`
+          <div id ='member_search_result'>
+          <div class='chat-group-user clearfix'>
+            <p class='chat-group-user__name'>${ name }</p>
+            <input name='group[user_ids][]' type='hidden' value=${ user_id }>
+            <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除
+            </div>
           </div>
-        </div>
-        </div>`  
+          </div>`  
 
-    member_list.append(html);
-  }
+      member_list.append(html);
+    }
 
-  function appendNoUsers(info) {
-    var html =`<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${ info }</p>
-              </div>`
+    function appendNoUsers(info) {
+      var html =`<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ info }</p>
+                </div>`
 
-    search_list.append(html);
-  }
+      search_list.append(html);
+    }
 
-  $(function(){
-    $("#user-search-field").on("keyup", function() {
-      var input = $("#user-search-field").val();
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data: { keyword: input },
-        dataType: 'json'
-      })
+    // $(function(){
+          //   $('#member_search_result').ready(function() {
 
-      .done(function(users) {
-        $("#user-search-result").empty();
-          if (users.length !== 0) {
-            users.forEach(function(user){
-            appendUsers(user);
-            });
-          }
-          else {
-            appendNoUsers("一致するユーザーはいません");
-          }
-        })
-      .fail(function() {
-        alert('ユーザー検索に失敗しました');
-      })
-    });
+            //     $.ajax({
+            //       type: 'GET',
+            //       url: '/group',
+            //       data: { keyword: input },
+            //       dataType: 'json'
+            //     })
+        
+            //     .done(function(users) {
+            //       $('#member_search_result').empty();
+            //         if (users.length !== 0) {
+            //           users.forEach(function(user){
+            //             appendMembers(name, user_id);
+            //           });
+            //         }
+            //         else {
+            //           appendNoUsers("一致するユーザーはいません");
+            //         }
+            //       })
+            //     .fail(function() {
+            //       alert('ユーザー検索に失敗しました');
+            //     })
+            //   });
+        
+            //   $(function(){
+            //     $(document).on('click', '.user-search-add', function() {
+            //       var name = $(this).data("user-name");
+            //       var user_id = $(this).data("user-id");
+            //       $(this).parent().remove();
+            //       appendMembers(name, user_id);
+            //     });
+        
+            //     $(document).on("click", '.user-search-remove', function() {
+            //       $(this).parent().remove();
+            //     });
+            //   });
+            // });
+    
 
     $(function(){
-      $(document).on('click', '.user-search-add', function() {
-        var name = $(this).data("user-name");
-        var user_id = $(this).data("user-id");
-        $(this).parent().remove();
-        appendMembers(name, user_id);
+      $("#user-search-field").on("keyup", function() {
+        var input = $("#user-search-field").val();
+        $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: { keyword: input },
+          dataType: 'json'
+        })
+
+        .done(function(users) {
+          $("#user-search-result").empty();
+            if (users.length !== 0) {
+              users.forEach(function(user){
+              appendUsers(user);
+              });
+            }
+            else {
+              appendNoUsers("一致するユーザーはいません");
+            }
+          })
+        .fail(function() {
+          alert('ユーザー検索に失敗しました');
+        })
       });
 
-      $(document).on("click", '.user-search-remove', function() {
-        $(this).parent().remove();
+      $(function(){
+        $(document).on('click', '.user-search-add', function() {
+          var name = $(this).data("user-name");
+          var user_id = $(this).data("user-id");
+          $(this).parent().remove();
+          appendMembers(name, user_id);
+        });
+
+        $(document).on("click", '.user-search-remove', function() {
+          $(this).parent().remove();
+        });
       });
     });
   });
-});
 });
